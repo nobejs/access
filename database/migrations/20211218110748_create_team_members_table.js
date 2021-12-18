d37aafac0c5e3bcbe73028b4bf0c6421ada4b4aa
Shubham Joshi <shubham@betalectic.com>
2021-12-18 17:12:32 +0530

    removed old create team_members migration, added new one. Deleted old migration entry from database

--- database/migrations/20211218110748_create_team_members_table.js
+++ database/migrations/20211218110748_create_team_members_table.js
@@ -7,9 +7,11 @@ exports.up = async function (knex) {
       .defaultTo(knex.raw("uuid_generate_v4()"));
     table.uuid("team_uuid").notNullable();
     table.uuid("user_uuid").nullable();
-    table.string("email", 255).nullable();
+    table.string("attribute_type", 255).nullable();
+    table.string("attribute_value", 255).nullable();
     table.string("status", 255).notNullable().defaultTo("invited");
-    table.string("role", 255).notNullable().defaultTo("member");
+    table.string("role_uuid", 255).nullable().defaultTo(null);
+    table.jsonb("permissions", 255).nullable().defaultTo(null);
     table.datetime("created_at", { useTz: false });
     table.datetime("updated_at", { useTz: false });
     table.datetime("deleted_at", { useTz: false });
