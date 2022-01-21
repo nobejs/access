a9df440bc2e6c324cbc486b5621c4c17947a4250
Rajiv Seelam <rajiv@betalectic.com>
2022-01-21 16:09:05 +0530

    Disable tests

--- src/stories/Users/LoginWithGoogle/tests.spec.js
+++ src/stories/Users/LoginWithGoogle/tests.spec.js
@@ -2,7 +2,7 @@ const debugLogger = requireUtil("debugLogger");
 const knex = requireKnex();
 
 describe("Test Handler Users/LoginWithGoogle", () => {
-  it("dummy_story_which_will_pass", async () => {
+  it.skip("dummy_story_which_will_pass", async () => {
     let result = {};
     try {
       result = await testStrategy("Users/LoginWithGoogle", {
--- src/stories/Users/RedirectForLoginWithGoogle/endpoint.spec.js
+++ src/stories/Users/RedirectForLoginWithGoogle/endpoint.spec.js
@@ -11,7 +11,7 @@ describe("Test API Users/RedirectForLoginWithGoogle", () => {
     };
   });
 
-  it("dummy_story_which_will_pass", async () => {
+  it.skip("dummy_story_which_will_pass", async () => {
     let respondResult;
     try {
       const app = httpServer();
