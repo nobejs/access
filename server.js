474a46d61e643238aac211f9b4a9bed6d5e8d3c3
Rajiv Seelam <rajiv@betalectic.com>
2022-03-11 18:37:13 +0530

    Handle cors entirely without another package

--- server.js
+++ server.js
@@ -15,30 +15,36 @@ const server = httpServer({
   },
 });
 
-server.addHook("onSend", function (request, reply, payload, next) {
+server.addHook("onRequest", function (request, reply, next) {
   console.log("request.url", request.url, request.method);
   console.log("request.headers.origin", request.headers.origin);
-  // console.log("request.headers", request.headers);
-
-  if (request.method === "OPTIONS") {
-    reply.header("Content-Type", "text/plain charset=UTF-8");
-    reply.header("Content-Length", "0");
-    reply.header("Access-Control-Max-Age", "1728000");
-  }
 
   if (request.headers.origin) {
     reply.header("Access-Control-Allow-Origin", request.headers.origin);
   }
 
-  next();
+  reply.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, OPTIONS");
+  reply.header("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,X-Client-Identifier,X-Team-Identifier");
+  reply.header("Access-Control-Allow-Credentials", "true");
+
+  if (request.method === "OPTIONS") {
+    reply.header("Content-Type", "text/plain charset=UTF-8");
+    reply.header("Content-Length", "0");
+    reply.header("Access-Control-Max-Age", "1728000");
+    reply.code(204)
+    reply.send();
+  } else {
+    next();
+  }
+
 });
 
-server.register(require('fastify-cors'), {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-  allowedHeaders: "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,X-Client-Identifier,X-Team-Identifier,Access-Control-Allow-Origin",
-  credentials: true,
-})
+// server.register(require('fastify-cors'), {
+//   origin: "*",
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+//   allowedHeaders: "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,X-Client-Identifier,X-Team-Identifier,Access-Control-Allow-Origin",
+//   credentials: true,
+// })
 
 // No 'Access-Control-Allow-Origin' header is present on the requested resource.
 
--- src/functions/authMiddleware.js
+++ src/functions/authMiddleware.js
@@ -14,62 +14,56 @@ const exclude = Config["excludeFromAuth"];
 
 module.exports = async (req, reply) => {
 
-  if (req.method === "OPTIONS") {
-    reply.header("Content-Type", "text/plain charset=UTF-8");
-    reply.header("Content-Length", "0");
-    reply.header("Access-Control-Max-Age", "1728000");
-    reply.code(204)
-    reply.send()
-  }
+  if (req.method !== "OPTIONS") {
 
-  let needsAuth = true;
-  // console.log("req.routerPath", req.method, req.url);
+    let needsAuth = true;
+    // console.log("req.routerPath", req.method, req.url);
 
-  if (req.routerPath === undefined) {
-    return reply.code(404).send({ error: "Not Found" });
-  }
-
-  exclude.forEach((p) => {
-    let [method, path] = p.split(" ");
-    let regex = pathToRegexp(path);
-    if (method == req.method && regex.exec(req.routerPath) !== null) {
-      needsAuth = false;
+    if (req.routerPath === undefined) {
+      return reply.code(404).send({ error: "Not Found" });
     }
-  });
 
-  // console.log("needsAuth", needsAuth);
+    exclude.forEach((p) => {
+      let [method, path] = p.split(" ");
+      let regex = pathToRegexp(path);
+      if (method == req.method && regex.exec(req.routerPath) !== null) {
+        needsAuth = false;
+      }
+    });
 
-  if (needsAuth) {
-    debugLogger("Header", req.headers.authorization);
+    // console.log("needsAuth", needsAuth);
 
-    if (!req.headers.authorization) {
-      debugLogger(
-        "Doesn't have authorization header",
-        req.headers.authorization
-      );
-      return reply.code(401).send({ error: "Unauthenticated" });
-    }
-    const bearer = req.headers.authorization.split(" ");
-    const bearerToken = bearer[1];
-    req.token = bearerToken;
+    if (needsAuth) {
+      debugLogger("Header", req.headers.authorization);
 
-    try {
-      let decoded = await decodeJWT(req.token);
-      await tokensRepo.checkIfValidJti(decoded.jti);
-      if (!decoded.sub || !decoded.jti || !decoded.iss) {
-        debugLogger("Missing one of sub, hti, issuer", decoded);
+      if (!req.headers.authorization) {
+        debugLogger(
+          "Doesn't have authorization header",
+          req.headers.authorization
+        );
+        return reply.code(401).send({ error: "Unauthenticated" });
+      }
+      const bearer = req.headers.authorization.split(" ");
+      const bearerToken = bearer[1];
+      req.token = bearerToken;
+
+      try {
+        let decoded = await decodeJWT(req.token);
+        await tokensRepo.checkIfValidJti(decoded.jti);
+        if (!decoded.sub || !decoded.jti || !decoded.iss) {
+          debugLogger("Missing one of sub, hti, issuer", decoded);
+          return reply.code(401).send({ message: "Unauthenticated" });
+        }
+        req.decodedJWT = decoded;
+        req.sub = decoded.sub;
+        req.jti = decoded.jti;
+        req.issuer = decoded.iss;
+      } catch (error) {
+        debugLogger(JSON.stringify(error));
+        debugLogger("Something went wrong in middleware", error);
         return reply.code(401).send({ message: "Unauthenticated" });
       }
-      req.decodedJWT = decoded;
-      req.sub = decoded.sub;
-      req.jti = decoded.jti;
-      req.issuer = decoded.iss;
-    } catch (error) {
-      debugLogger(JSON.stringify(error));
-      debugLogger("Something went wrong in middleware", error);
-      return reply.code(401).send({ message: "Unauthenticated" });
     }
   }
-
   //   next();
 };
