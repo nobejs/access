8cbc92acd8c49db4a96e25378106101acc3fa4ee
praveen-betalectic <102508587+praveen-betalectic@users.noreply.github.com>
2022-10-19 11:57:37 +0530

    delete admin by super user implemented

---
+++ src/stories/Admin/CanDeleteAdmin/story.js
@@ -0,0 +1,66 @@
+const validator = requireValidator();
+const adminRepo = requireRepo("admin");
+const tokensRepo = requireRepo("tokens");
+const findKeysFromRequest = requireUtil("findKeysFromRequest");
+
+const prepare = ({ reqQuery, reqBody, reqParams, req }) => {
+  const payload = findKeysFromRequest(req, ["admin_uuid"]);
+  payload["jti"] = req.jti;
+  return payload;
+};
+
+const authorize = async ({ prepareResult }) => {
+  let unauthorizedObject = {
+    statusCode: 401,
+    message: "Unauthorized",
+  };
+
+  try {
+    if (prepareResult.jti !== undefined) {
+      let token = await tokensRepo.first({
+        uuid: prepareResult.jti,
+      });
+      if (token["issuer"] === "admin") {
+        let isSuperUser = await adminRepo.isSuperUser(token.sub);
+        if (isSuperUser) {
+          return token;
+        } else {
+          throw unauthorizedObject;
+        }
+      } else {
+        throw unauthorizedObject;
+      }
+    } else {
+      throw unauthorizedObject;
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const handle = async ({ prepareResult, authorizeResult }) => {
+  try {
+    let payload = {
+      logedAdminUuid: authorizeResult.uuid,
+      deleteAdminUuid: prepareResult.admin_uuid,
+    };
+    return await adminRepo.deleteAdmin(payload);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const respond = async ({ handleResult }) => {
+  try {
+    return handleResult;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  prepare,
+  authorize,
+  handle,
+  respond,
+};
