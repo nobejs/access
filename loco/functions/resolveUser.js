ed89b4f2622d60b22f33f67d8e5850adf45706ec
Rajiv Seelam <rajiv@betalectic.com>
2022-10-31 16:40:56 +0530

    Add admin-team-members json

--- loco/functions/resolveUser.js
+++ loco/functions/resolveUser.js
@@ -7,6 +7,30 @@ const resolveUser = async (locoRoute, frameworkData) => {
     }
   }
 
+  if (locoRoute.resource === "admin-attributes") {
+    if (frameworkData.req.issuer === "admin") {
+      return "*";
+    } else {
+      return [];
+    }
+  }
+
+  if (locoRoute.resource === "admin-teams") {
+    if (frameworkData.req.issuer === "admin") {
+      return "*";
+    } else {
+      return [];
+    }
+  }
+
+  if (locoRoute.resource === "admin-team-members") {
+    if (frameworkData.req.issuer === "admin") {
+      return "*";
+    } else {
+      return [];
+    }
+  }
+
   return "*";
 };
 
