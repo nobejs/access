05817cf4fe8e096687e1fdcb9484d615a7611839
rajeshn95 <rajesh@betalectic.com>
2023-05-04 20:29:06 +0530

    template changes

--- src/functions/whatsAppSendMessage.js
+++ src/functions/whatsAppSendMessage.js
@@ -3,39 +3,33 @@ module.exports = async (payload) => {
   var data = JSON.stringify({
     messaging_product: "whatsapp",
     to: payload.mobile,
-    // enable template if
-    // type: "template",
-    // template: {
-    //   name: "redirect_with_token",
-    //   language: {
-    //     code: "en",
-    //   },
-    //   components: [
-    //     {
-    //       type: "header",
-    //       parameters: [{ type: "text", text: process.env.WHATSAPP_APP_NAME }],
-    //     },
-    //     {
-    //       type: "body",
-    //       parameters: [{ type: "text", text: process.env.WHATSAPP_APP_NAME }],
-    //     },
-    //     {
-    //       type: "button",
-    //       sub_type: "url",
-    //       index: "0",
-    //       parameters: [
-    //         {
-    //           type: "text",
-    //           text: payload.token,
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
-    type: "text",
-    preview_url: true,
-    text: {
-      body: `Please click the link to login: ${process.env.WHATSAPP_REDIRECT_URL}?access_token=${payload.token}`,
+    type: "template",
+    template: {
+      name: "redirect_with_token",
+      language: {
+        code: "en",
+      },
+      components: [
+        {
+          type: "header",
+          parameters: [{ type: "text", text: process.env.WHATSAPP_APP_NAME }],
+        },
+        {
+          type: "body",
+          parameters: [{ type: "text", text: process.env.WHATSAPP_APP_NAME }],
+        },
+        {
+          type: "button",
+          sub_type: "url",
+          index: "0",
+          parameters: [
+            {
+              type: "text",
+              text: payload.token,
+            },
+          ],
+        },
+      ],
     },
   });
 
