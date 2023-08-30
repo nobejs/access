1a9da58ad60c8f044543a7753dbf9a166c902ffb
Arafat Alim <126658754+arafatalim@users.noreply.github.com>
2023-08-30 12:09:38 +0530

    Added Request Logs in server.js. (#55)
    * Added Static Access logs and removed the JSON prettifier parameters from the JSON.stringify.
    
    * changed the static key name.
    
    * removed the conditional checking "error status code".

--- server.js
+++ server.js
@@ -25,36 +25,32 @@ server.addHook("onRequest", async (req, reply) => {
 
 server.addHook("onSend", function (request, reply, payload, next) {
   if (process.env.ENABLE_REQUEST_LOGGING === "true") {
-    if (reply.statusCode === 401 || request.is404 || reply.statusCode === 404) {
-      // Handle if condition
-    } else {
-      let apiLog = {
-        timestamp: new Date().toISOString(),
-        status_code: reply.statusCode,
-        http_method: request.method,
-        url: `${request.hostname}${request.url}`,
-        hostname: request.hostname,
-        protocol: request.protocol,
-        source: {
-          ip: request.ip,
-          user_agent: request.headers["user-agent"],
-        },
-        request_payload: JSON.stringify(request.body),
-        response: payload,
-        user_uuid: request.user,
-        uuid: request.user ? request.user : "",
-        meta: {
-          id: request.id,
-        },
-      };
+    let apiLog = {
+      timestamp: new Date().toISOString(),
+      status_code: reply.statusCode,
+      http_method: request.method,
+      url: `${request.hostname}${request.url}`,
+      hostname: request.hostname,
+      protocol: request.protocol,
+      source: {
+        ip: request.ip,
+        user_agent: request.headers["user-agent"],
+      },
+      request_payload: JSON.stringify(request.body),
+      response: payload,
+      user_uuid: request.user,
+      uuid: request.user ? request.user : "",
+      meta: {
+        id: request.id,
+      },
+    };
 
-      console.log(
-        `${request.hostname}${request.url}-${new Date().toISOString()}${
-          request.user ? `-${request.user}` : ""
-        }`,
-        JSON.stringify(apiLog, null, 2)
-      );
-    }
+    console.log(
+      `ACCESS_API_LOG_${request.hostname}${
+        request.url
+      }-${new Date().toISOString()}${request.user ? `-${request.user}` : ""}`,
+      JSON.stringify(apiLog)
+    );
   }
 
   next();
