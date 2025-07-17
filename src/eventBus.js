01dc003f8172e890e16f38e44070dcec517dae00
Avi060545 <avi@betalectic.com>
2025-07-17 18:17:18 +0530

    ist time correction

--- src/eventBus.js
+++ src/eventBus.js
@@ -310,11 +310,18 @@ const processUserRequestLoginOTP = async (eventData) => {
   };
 
   const now = new Date();
-  const formattedTime = `${String(now.getHours()).padStart(2, "0")}:${String(
-    now.getMinutes()
-  ).padStart(2, "0")}, ${String(now.getDate()).padStart(2, "0")}/${String(
-    now.getMonth() + 1
-  ).padStart(2, "0")}/${now.getFullYear()}`;
+  const istOffset = 5.5 * 60 * 60 * 1000; // 5 hours 30 minutes in ms
+  const istDate = new Date(now.getTime() + istOffset);
+
+  const formattedTime = `${String(istDate.getUTCHours()).padStart(
+    2,
+    "0"
+  )}:${String(istDate.getUTCMinutes()).padStart(2, "0")}, ${String(
+    istDate.getUTCDate()
+  ).padStart(2, "0")}/${String(istDate.getUTCMonth() + 1).padStart(
+    2,
+    "0"
+  )}/${istDate.getUTCFullYear()}`;
 
   let data = {
     token: eventObject.token,
